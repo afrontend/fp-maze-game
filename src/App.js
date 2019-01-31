afd463d215d48d46583c497e199e4b4a4366ac14
afrontend <frontend.wordpress@gmail.com>
2019-01-31 22:10:19 +0900

    add blocks (20x20)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
+/*
+JavaScript Maze
+By Bob Hwang
+https://github.com/afrontend/fp-maze-game
+*/
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import _ from 'lodash';
 import './App.css';
+
+// configuration
+
+const CONFIG = {
+  rows: 20,
+  columns: 20,
+  color: 'grey'
+};
+
+// panel functions
+
+const getAry = (len, fn) => (
+  _.range(len).map(() => (
+    fn
+    ? (
+      _.isFunction(fn)
+      ? fn()
+      : fn )
+    : null)
+  ));
+
+const createItem = () => ({ color: CONFIG.color });
+const getEmptyRow = () => (getAry(CONFIG.columns, createItem));
+const createPanel = () => (getAry(CONFIG.rows, getEmptyRow));
+const convert1DimAry = _.flattenDepth;
+
+console.log(createPanel());
+
+const Block = props => (
+  <div className="block" style={{backgroundColor: props.color}}>
+    {props.children}
+  </div>
+);
+
+const Blocks = props => (createBlocks(props.window));
+
+const createBlocks = ary => (
+  ary.map(
+    (item, index) => (
+      <Block color={item.color} key={index}>
+        {item.count}
+      </Block>
+    )
+  )
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="App">
+          <Blocks window={convert1DimAry(createPanel())} />
+        </div>
       </div>
     );
   }
