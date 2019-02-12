641d7adf255ccfc6ac0db8450318c43fe3b33cb2
afrontend <frontend.wordpress@gmail.com>
2019-02-12 22:49:35 +0900

    paint wall of block

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ https://github.com/afrontend/fp-maze-game
 
 import React, { Component } from 'react';
 import _ from 'lodash';
+import fp from 'lodash/fp';
 import './App.css';
 
 // configuration
 
 const CONFIG = {
-  rows: 20,
-  columns: 20,
+  rows: 25,
+  columns: 25,
   color: 'grey' ,
   pathColor: 'blue'
 };
@@ -33,12 +34,7 @@ const createItem = () => ({ color: CONFIG.color });
 const getEmptyRow = () => (getAry(CONFIG.columns, createItem));
 const createPanel = () => (getAry(CONFIG.rows, getEmptyRow));
 const convert1DimAry = _.flattenDepth;
-
-const Block = props => (
-  <div className="block" style={{backgroundColor: props.color}}>
-    {props.children}
-  </div>
-);
+const convert2DimAry = fp.chunk(CONFIG.columns)
 
 // paint
 
@@ -54,28 +50,68 @@ const paint = (panel, posAry, color) => {
   return newPanel;
 };
 
+// path
+
+const paintPath = panel => {
+  return paint(panel, [{
+    row: 12,
+    column: 12
+  }], CONFIG.pathColor);
+};
+
+const updatePath = f => f;
+
+/*
 const paintPath = panel => {
   return paint(panel, [{
     row: _.random(0, panel.length - 1),
     column: _.random(0, panel[0].length - 1)
   }], CONFIG.pathColor);
 };
+*/
 
 // react components
 
-const Blocks = props => (createBlocks(props.window));
+const getWall = props => {
+  const className = ['block'];
+  if(props.top) className.push('top');
+  if(props.right) className.push('right');
+  if(props.bottom) className.push('bottom');
+  if(props.left) className.push('left');
+  return className.join(' ');
+};
+
+const Block = props => (
+  <div className={getWall(props.item)} style={{backgroundColor: props.color}}>
+    {props.children}
+  </div>
+);
 
 const createBlocks = ary => (
   ary.map(
     (item, index) => (
-      <Block color={item.color} key={index}>
+      <Block item={item} color={item.color} key={index}>
         {item.count}
       </Block>
     )
   )
 );
 
-const createPathPanel = _.flow([createPanel, paintPath]);
+const Blocks = props => (createBlocks(props.window));
+
+const addWall = panel => {
+  const newPanel = _.cloneDeep(panel);
+  const ary =  _.map(convert1DimAry(newPanel), (item) => {
+    item.top = true;
+    item.right = true;
+    item.bottom = true;
+    item.left = true;
+    return item;
+  });
+  return convert2DimAry(ary);
+}
+
+const createPathPanel = _.flow([createPanel, paintPath, addWall]);
 
 class App extends Component {
   constructor(props) {
@@ -83,6 +119,14 @@ class App extends Component {
     this.state = {
       pathPanel: createPathPanel(),
     };
+
+    this.state.timer = setInterval(() => {
+      this.setState((state) => {
+        return {
+          pathPanel: updatePath(state.pathPanel),
+        };
+      });
+    }, 1000);
   }
 
   render() {
