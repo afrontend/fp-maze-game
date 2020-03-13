33f5490e19e103cd9de33b604c0b8a6783ff2a38
afrontend <autoscripts@gmail.com>
2020-03-13 23:04:07 +0900

    Updates

---
+++ precache-manifest.63a566955a9e564a608530d8f7bc396c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "635c652108ab47b86dce",
+    "url": "/fp-maze-game/static/js/runtime~main.635c6521.js"
+  },
+  {
+    "revision": "19b4c162a53234d3dacc",
+    "url": "/fp-maze-game/static/js/main.19b4c162.chunk.js"
+  },
+  {
+    "revision": "40e6e973f627aff7cd18",
+    "url": "/fp-maze-game/static/js/1.40e6e973.chunk.js"
+  },
+  {
+    "revision": "19b4c162a53234d3dacc",
+    "url": "/fp-maze-game/static/css/main.bbef8128.chunk.css"
+  },
+  {
+    "revision": "721759018b78e8edc6f74d5dafb79fbd",
+    "url": "/fp-maze-game/index.html"
+  }
+];
